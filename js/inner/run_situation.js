ca9f5faea5861c42f6008da80f42956defa3fad5
Ziv.zeng <Ziv.zeng@embracesource.com>
2017-03-20 18:47:56 +0800

    删除多余js

--- js/inner/run_situation.js
+++ js/inner/run_situation.js
@@ -53,10 +53,3 @@ $(function () {
         if (r != null) return unescape(r[2]); return null; //返回参数值
     }
 })
-
-    //获取浏览器参数方法
-    function getUrlParam(name) {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-        var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-        if (r != null) return unescape(r[2]); return null; //返回参数值
-    }
